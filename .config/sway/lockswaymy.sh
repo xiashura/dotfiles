6688487cb191d5b42e85bdb89506c0afccac0934
xiashura <plexanov@protonmail.com>
2021-02-16 22:26:01 +0500

    update

---
+++ .config/sway/lockswaymy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wal=$(cat ~/.cache/wal/wal)
+swaylock -f -i $wal
---
+++ .config/sway/touchpad.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+#STATE=`swaymsg -t get_inputs | grep $ID  `
+
+# echo $STATE
+# if [ $STATE -eq 1 ]
+# then
+#     xinput disable $ID
+# 		killall fusuma
+#     notify-send "Touchpad disabled."
+# else
+#     xinput enable $ID
+# 		notify-send "Touchpad enabled."
+# 		fusuma --config=~/.config/fusuma/config.yml  
+# fi
